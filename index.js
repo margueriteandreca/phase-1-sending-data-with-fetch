e832718ce5360a2b0a109aad5dc3d82b21b13fe8
+ <+>
2022-06-10 13:40:27 -0400

    commit tree ef4abe396d59b681ecae6cab848dd1092e2130a5

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+/*const nameAndEmail = {
+    name: "Marguerite"
+    email: "margueriteandreca@gmail.com"
+}
+
+*/
+
+function submitData() {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({name: "Marguerite", email: "margueriteandreca@gmail.com"})
+    })
+    .then(res => res.json())
+    .then(function (data) {
+        const p = document.createElement("p");
+        p.textContent = data.id
+        document.body.appendChild(p)
+    })
+    .catch(error => {
+        console.log(error);
+        const ourError = document.createElement("p");
+        ourError.textContent = error.message 
+        document.body.appendChild(ourError)});
+    };
